515156dd1b16b2d47dc93f83f36adf97a7d9204a
Alfrick Opidi <alfo.opidi85@gmail.com>
2017-06-19 19:30:16 +0300

    connecting to a database

--- connect.php
+++ connect.php
@@ -4,6 +4,7 @@ $msql_host = 'localhost';
 $msql_user = 'j';
 $msql_pass = 'j85';
 
+
 @$msql_con = mysqli_connect($msql_host, $msql_user, $msql_pass) or die('Database connection unsucessful');
 
 mysqli_select_db($msql_con, 'new_database') or die('Database not found');
