5af3c0f1348522083cd11ae13b0eb277683009cd
alfo.opidi85@gmail.com <alfo.opidi@gmail.com>
2017-06-19 18:07:45 +0300

    including registration details

---
+++ indexfour.php
@@ -0,0 +1,76 @@
+<?php
+
+$mysqli_con = mysqli_connect('localhost', 'root','','the_database');
+	
+$current_file = $_SERVER['SCRIPT_NAME'];
+
+ob_start();
+session_start();
+
+function loggedIn(){
+	global $mysqli_con;
+if(isset($_POST['username']) && isset($_POST['password'])){
+	if(!empty($_POST['username']) && !empty($_POST['password'])){
+		$username = @mysqli_real_escape_string($mysqli_con, $_POST['username']);//protecting against SQL injection
+		$password = @mysqli_real_escape_string($mysqli_con, $_POST['password']);
+		$password_hash = md5($password);
+
+		$query_select = "SELECT id FROM users_logging WHERE username='$username' AND password= '$password_hash'";
+
+		if($query = @mysqli_query($mysqli_con, $query_select)){
+
+			if(mysqli_num_rows($query)== 0){
+				echo 'Invalid login details';
+			}else if(mysqli_num_rows($query)==1){
+				$user_id = mysqli_fetch_assoc($query);
+				foreach($user_id as $data){
+					$data;
+				}
+				$_SESSION['user_id'] = $data;
+				//header('Location: index.php');
+					if(isset($data) && !empty($data)){
+						$firstname = getUserField('firstname');
+						$surname = getUserField('surname');
+					echo 'You are logged in ' .$firstname. ' ' .$surname. ' <a href="pagetwo.php">Logout</a><br>';
+					
+					}
+
+			}
+		}
+
+
+	}else{
+		echo 'Please provide username and password';
+	}
+}
+	return true;
+}
+
+
+function getUserField($field){
+	global $mysqli_con;
+	$sess = $_SESSION['user_id'];
+	$select = "SELECT $field FROM users_logging WHERE id = '$sess'";
+	if($query = mysqli_query($mysqli_con, $select)){
+		if($query_result= mysqli_fetch_assoc($query)){
+			foreach($query_result as $data){
+				return $data;
+			}
+		}
+	}
+}
+
+
+loggedIn();
+
+mysqli_close($mysqli_con);//closing the connection is important
+
+?>
+
+<form action="<?php echo $current_file; ?>" method="POST">
+
+Username:<input type="text" name="username" />
+Password:<input type="password" name="password" />
+			<input type="submit" value="Login" />
+</form>
+
